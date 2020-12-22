2701397618146042c427fe0a2defbb3574d6c031
Zhe Ren <zxr45@case.edu>
2020-12-22 10:42:14 -0500

    committing my first files, including Rproj and gitignore. Of couse, I can selectively choose a specific one, but I just want to commit them all this time.

---
+++ lession-01.R
@@ -0,0 +1,8 @@
+# git add: adds modified files to the queue to be committed.
+# git commit: commits the files that has been added and creates a new version of log.
+
+# git push: pushes (shares) changes to the remote repo (server), e.g., github. 
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+options(digits = 3)
